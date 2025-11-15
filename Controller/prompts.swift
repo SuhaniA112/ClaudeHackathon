664babbabcb7d96fae8eac36fa447607dcd9b724
SuhaniA112 <saggarwal1219@gmail.com>
2025-11-15 13:25:57 -0500

    actual changes and skeleton oops

--- Controller/prompts.swift
+++ Controller/prompts.swift
@@ -8,12 +8,15 @@
 import SwiftAnthropic
 import UIKit
 
+package model
 
 func getFeedback(img: UIImage, usr : Int) -> String {
     
     prompt = getPrompt(img, usr)
+    // get msg
     
-   // change return msg to return ai thing
+    // parse msg, add data to model etc.
+   // return relevant content
     return msg;
 }
 
@@ -23,11 +26,22 @@ func getPrompt(img: UIImage, usr: Int) -> String {
     // fetch: user age, user weight, user past data, user health goal
     let base64Image = img.base64EncodedString()
     
-    age = 0;
-    weight = 0;
-    past = [];
-    healthGoal = "";
+    age = model.getAge(usr);
+    weight = model.getWeight(usr);
+    exercise = model.getExercise(usr);
+    gender = model.getGender(usr);
+    restrictions = model.getRestrictions(usr);
+    past = []; // TODO
+    healthGoal = model.getGoal(usr);
     
-    String prompt = "For this image what is their.. " + "For an individual with age " + age + " and weight " + weight + " pounds and a health goal of " + health goal + " how healthy is their meal on a scale of 1 - 10? " + "Also, what are some suggestions for improving this meal?"
+    if (gender is "prefer not to say") {
+        String prompt = "For this image what rate their food proprtions in terms of protein, carbohydrates, and fat consumption. Also consider any other nutritional factors that may be important. Provide me with 4 numbers, each on a scale of 1-10 corresponding to each category listed. The food image is: " + base64Image + ". For an individual with age " + age + " and weight " + weight + " pounds and exercises " + exercise + " times per week and a health goal of " + health goal + " how healthy is their meal on a scale of 1 - 10? " + "Also, what are some suggestions for improving this meal? They're dietary restrictions are " + restrictions + "Additionally, this the average of their past weeks in the same format of 4 numbers, how does today compare to that: " + past
+    } else {
+        String prompt = "For this image what rate their food proprtions in terms of protein, carbohydrates, and fat consumption. Also consider any other nutritional factors that may be important. Provide me with 4 numbers, each on a scale of 1-10 corresponding to each category listed. The food image is: " + base64Image + ". For an individual of gender " + gender + " with age " + age + " and weight " + weight + " pounds and exercises " + exercise + " times per week and a health goal of " + health goal + " how healthy is their meal on a scale of 1 - 10? " + "Also, what are some suggestions for improving this meal? They're dietary restrictions are " + restrictions + "Additionally, this the average of their past weeks in the same format of 4 numbers, how does today compare to that: " + past
+    }
+    
+    
     
 }
+
+
--- model/data.swift
+++ model/data.swift
@@ -5,3 +5,26 @@
 //  Created by Suhani Aggarwal on 11/15/25.
 //
 
+func getAge(uid: Int) -> Int {
+    
+}
+
+func getWeight(uid: Int) -> Int {
+    
+}
+
+func getExericse(uid: Int) -> Double{
+    
+}
+
+func getGender(uid: Int) -> String {
+    
+}
+
+func getRestriction(uid: Int) -> String {
+    
+}
+
+func getGoal(uid: Int) -> String {
+    
+}
