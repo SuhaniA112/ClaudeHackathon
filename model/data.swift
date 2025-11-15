fd166d1a0bac1b5bcef0fabdec3e5e7d12aa1a75
SuhaniA112 <saggarwal1219@gmail.com>
2025-11-15 13:31:08 -0500

    moved getters

--- model/data.swift
+++ model/data.swift
@@ -5,26 +5,3 @@
 //  Created by Suhani Aggarwal on 11/15/25.
 //
 
-func getAge(uid: Int) -> Int {
-    
-}
-
-func getWeight(uid: Int) -> Int {
-    
-}
-
-func getExericse(uid: Int) -> Double{
-    
-}
-
-func getGender(uid: Int) -> String {
-    
-}
-
-func getRestriction(uid: Int) -> String {
-    
-}
-
-func getGoal(uid: Int) -> String {
-    
-}
---
+++ model/get.swift
@@ -0,0 +1,30 @@
+//
+//  get.swift
+//  
+//
+//  Created by Suhani Aggarwal on 11/15/25.
+//
+
+func getAge(uid: Int) -> Int {
+    
+}
+
+func getWeight(uid: Int) -> Int {
+    
+}
+
+func getExericse(uid: Int) -> Double{
+    
+}
+
+func getGender(uid: Int) -> String {
+    
+}
+
+func getRestriction(uid: Int) -> String {
+    
+}
+
+func getGoal(uid: Int) -> String {
+    
+}
